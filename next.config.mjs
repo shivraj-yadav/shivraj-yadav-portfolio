90a1aa84c410b64e78fd692aec788357feea8854
Shivraj  Yadav <114981686+shivraj-yadav@users.noreply.github.com>
2026-01-31 23:00:00 +0530

    Refactor next.config.mjs for output and typescript

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: process.env.STATIC_EXPORT === "true" ? "export" : undefined,
+
   images: {
     unoptimized: true,
   },
-  onDemandEntries: {
-    maxInactiveAge: 1000,
-    maxSize: 50,
-  },
-}
 
-export default nextConfig
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
